5e6bfef1d86163e908a65a6ea4e72c62d6fd7117
Artyom <ag.energy@mail.ru>
2022-03-31 02:25:35 +0300

    changes

---
+++ backend/shortener/test/server_test.go
@@ -0,0 +1,52 @@
+package test
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"testing"
+	u "url_shortener/backend/lib"
+	"url_shortener/backend/shortener/api"
+	"url_shortener/backend/shortener/internal/lib"
+)
+
+func TestServer(t *testing.T) {
+	config := u.NewConfig("../cmd/config.yml")
+	server := lib.NewServer(config)
+	server.ConfigureRouter()
+
+	if err := server.ConnectToDB(); err != nil {
+		t.Fatal(err)
+	}
+	if err := server.Run(); err != nil {
+		t.Fatal(err)
+	}
+
+	// test request
+
+	url := "http://localhost:9000/create"
+
+	r := api.ReqCreateShortUrl{
+		UrlLong: "wwwww",
+	}
+	js, _ := json.Marshal(r)
+	req, _ := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(js))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Test", "testheader")
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	header := resp.Header
+	t.Log("response Header: ", header)
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	t.Logf("response Body: %s", string(body))
+
+	server.Shutdown(context.Background())
+}
